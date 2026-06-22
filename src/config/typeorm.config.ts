bdf15f6bdbe3a7e6461994398324dc71549fe716
pokkie Le poh <lepohpokkie@gmail.com>
2026-06-22 20:03:26 +0700

    Configure Cloud SQL integration

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,8 +2,9 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import 'dotenv/config';
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT || 3306),
+  ...(process.env.DB_SOCKET_PATH
+    ? { socketPath: process.env.DB_SOCKET_PATH }
+    : { host: process.env.DB_HOST, port: Number(process.env.DB_PORT || 3306) }),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
